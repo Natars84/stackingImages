707d8f64938bb5f2ac85f70cdbe3d127e5f7c8c8
Natars <pasero.romain@gmail.com>
2025-12-01 18:45:07 +0900

    Correction des erreurs dans le script

--- install.sh
+++ install.sh
@@ -2,17 +2,13 @@
 
 # Package to install
 IMAGEMAGICK_PKG="imagemagick"
-PYTHON="python3"
-WAND="libmagickwand-dev python-wand"
+PYTHON="python3 python3-pip"
+WAND="libmagickwand-dev python3-wand"
 
 # Update package lists and install packages
 apt-get update --yes
 apt-get upgrade --yes
 apt-get install --yes $IMAGEMAGICK_PKG $WAND $PYTHON
 
-# Install pip for Python 3
-python -m ensurepip --upgrade
-wget https://bootstrap.pypa.io/get-pip.py | $PYTHON
-
 # Install Wand Python library
-$PYTHON -m pip install Wand
+python3 -m pip install Wand
